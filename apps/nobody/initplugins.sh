393660429d07cd399f580176561f513aa9dcb933
binhex <megalith01@gmail.com>
2017-02-27 18:08:19 +0000

    minor tweaks to info logging

--- apps/nobody/initplugins.sh
+++ apps/nobody/initplugins.sh
@@ -7,21 +7,24 @@ if [[ "${ENABLE_FLOOD}" == "yes" ]]; then
 
 else
 
-	echo "[info] Waiting for rTorrent and nginx to start..."
+	echo "[info] Initialising ruTorrent plugins (checking rTorrent is running)..."
 
 	# wait for rtorrent process to start (listen for port)
 	while [[ $(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".5000"') == "" ]]; do
 		sleep 0.1
 	done
 
+	echo "[info] rTorrent running"
+	echo "[info] Initialising ruTorrent plugins (checking nginx is running)..."
+	
 	# wait for nginx process to start (listen for port)
 	while [[ $(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".9080"') == "" ]]; do
 		sleep 0.1
 	done
 
-	echo "[info] rTorrent and nginx started"
-
+	echo "[info] nginx running"
 	echo "[info] Initialising ruTorrent plugins..."
+	
 	# run php plugins for rutorent (required for scheduler and rss feed plugins)
 	/usr/bin/php /usr/share/webapps/rutorrent/php/initplugins.php admin
 	echo "[info] ruTorrent plugins initialised"
