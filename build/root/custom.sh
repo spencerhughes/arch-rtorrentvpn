b4e9d449f746c0d3243508da723448e91a103210
binhex <megalith01@gmail.com>
2017-10-25 13:18:29 +0100

    switch to script pulldown on demand

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -9,11 +9,11 @@ install_name="flood"
 install_folder="/etc/webapps/flood"
 
 # find latest release tag from github
-curly.sh -rc 6 -rw 10 -of /tmp/release_tag -url "https://github.com/${repo_name}/${app_name}/releases"
+/root/curly.sh -rc 6 -rw 10 -of /tmp/release_tag -url "https://github.com/${repo_name}/${app_name}/releases"
 release_tag=$(cat /tmp/release_tag | grep -P -o -m 1 "(?<=/${repo_name}/${app_name}/releases/tag/)[^\"]+")
 
 # download install zip file
-curly.sh -rc 6 -rw 10 -of "/tmp/${app_name}-release.zip" -url "https://github.com/${repo_name}/${app_name}/archive/${release_tag}.zip"
+/root/curly.sh -rc 6 -rw 10 -of "/tmp/${app_name}-release.zip" -url "https://github.com/${repo_name}/${app_name}/archive/${release_tag}.zip"
 
 # unzip to /tmp
 unzip "/tmp/${app_name}-release.zip" -d /tmp
@@ -31,7 +31,7 @@ rm "/tmp/${app_name}-release.zip"
 cd "${install_folder}" && npm install --production
 
 # download htpasswd (problems with apache-tools and openssl 1.1.x)
-curly.sh -rc 6 -rw 10 -of /tmp/htpasswd.tar.gz -url "https://github.com/binhex/arch-packages/raw/master/compiled/htpasswd.tar.gz"
+/root/curly.sh -rc 6 -rw 10 -of /tmp/htpasswd.tar.gz -url "https://github.com/binhex/arch-packages/raw/master/compiled/htpasswd.tar.gz"
 
 # extract compiled version of htpasswd
 tar -xvf /tmp/htpasswd.tar.gz -C /
