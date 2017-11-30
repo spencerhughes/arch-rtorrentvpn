0e2c10ba7d087e5728c83bfc6e57908335de4ca8
binhex <megalith01@gmail.com>
2017-11-30 16:30:02 +0000

    silence stdout/stderr for ipv6 drop

--- run/root/iptable.sh
+++ run/root/iptable.sh
@@ -93,7 +93,7 @@ echo "[info] Docker network defined as ${docker_network_cidr}"
 iptables -P INPUT DROP
 
 # set policy to drop ipv6 for input
-ip6tables -P INPUT DROP
+ip6tables -P INPUT DROP 1>&- 2>&-
 
 # accept input to tunnel adapter
 iptables -A INPUT -i "${VPN_DEVICE_TYPE}"0 -j ACCEPT
@@ -147,7 +147,7 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -P OUTPUT DROP
 
 # set policy to drop ipv6 for output
-ip6tables -P OUTPUT DROP
+ip6tables -P OUTPUT DROP 1>&- 2>&-
 
 # accept output from tunnel adapter
 iptables -A OUTPUT -o "${VPN_DEVICE_TYPE}"0 -j ACCEPT
