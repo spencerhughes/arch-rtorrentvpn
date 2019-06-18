da390bd6035975aaa17107497e9a2e36a5322645
binhex <megalith01@gmail.com>
2019-06-18 13:12:31 +0100

    put in call to nginx log rotate script

--- run/nobody/logrotate.sh
+++ run/nobody/logrotate.sh
@@ -6,37 +6,43 @@ file_size_limit_kb=10240
 
 if [[ -f "${log_path}" ]]; then
 
-	file_size_kb=$(du -k "${log_path}" | cut -f1)
+	while true; do
 
-	if [ "${file_size_kb}" -ge "${file_size_limit_kb}" ]; then
+		file_size_kb=$(du -k "${log_path}" | cut -f1)
 
-		echo "[info] Nginx log file larger than limit ${file_size_limit_kb} kb, rotating logs..."
+		if [ "${file_size_kb}" -ge "${file_size_limit_kb}" ]; then
 
-		if [[ -f "${log_path}.${number_of_logs_to_keep}" ]]; then
-			echo "[info] Deleting oldest log file '${log_path}.${number_of_logs_to_keep}'..."
-			rm -f "${log_path}.${number_of_logs_to_keep}"
-		fi
+			echo "[info] Nginx log file larger than limit ${file_size_limit_kb} kb, rotating logs..."
 
-		for log_number in $(seq "${number_of_logs_to_keep}" -1 0); do
-
-			if [[ -f "${log_path}.${log_number}" ]]; then
-				log_number_inc=$((log_number+1))
-				mv "${log_path}.${log_number}" "${log_path}.${log_number_inc}"
+			if [[ -f "${log_path}.${number_of_logs_to_keep}" ]]; then
+				echo "[info] Deleting oldest log file '${log_path}.${number_of_logs_to_keep}'..."
+				rm -f "${log_path}.${number_of_logs_to_keep}"
 			fi
 
-		done
+			for log_number in $(seq "${number_of_logs_to_keep}" -1 0); do
 
-		echo "[info] Moving current log to ${log_path}.0..."
-		mv "${log_path}" "${log_path}.0"
+				if [[ -f "${log_path}.${log_number}" ]]; then
+					log_number_inc=$((log_number+1))
+					mv "${log_path}.${log_number}" "${log_path}.${log_number_inc}"
+				fi
 
-		echo "[info] Force Nginx to reload log files..."
-		kill -USR1 $(cat /home/nobody/nginx.pid)
-		sleep 1
+			done
 
-	fi
+			echo "[info] Moving current log to ${log_path}.0..."
+			mv "${log_path}" "${log_path}.0"
+
+			echo "[info] Force Nginx to reload log files..."
+			kill -USR1 $(cat /home/nobody/nginx.pid)
+			sleep 1
+
+		fi
+
+		sleep 30s
+
+	done
 
 else
 
-	echo "[info] Nginx log file does not exist, skipping log rotation"
+	echo "[info] Nginx log file does not exist in '${log_path}', skipping log rotation"
 
 fi
