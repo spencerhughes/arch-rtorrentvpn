73103b1261d3528c3cca447ff38bbd413dd93c87
binhex <megalith01@gmail.com>
2019-06-18 15:26:51 +0100

    better checks before attempting to rotate logs

--- run/nobody/logrotate.sh
+++ run/nobody/logrotate.sh
@@ -4,45 +4,47 @@ log_path="/config/access.log"
 number_of_logs_to_keep=3
 file_size_limit_kb=10240
 
-if [[ -f "${log_path}" ]]; then
+# wait for nginx process to start (listen for port)
+while [[ $(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".9080"') == "" ]]; do
+	sleep 0.1
+done
 
-	while true; do
+# wait for nginx to start logging access to file
+while [[ ! -f "${log_path}" ]]; do
+	sleep 0.1
+done
 
-		file_size_kb=$(du -k "${log_path}" | cut -f1)
+while true; do
 
-		if [ "${file_size_kb}" -ge "${file_size_limit_kb}" ]; then
+	file_size_kb=$(du -k "${log_path}" | cut -f1)
 
-			echo "[info] Nginx log file larger than limit ${file_size_limit_kb} kb, rotating logs..."
+	if [ "${file_size_kb}" -ge "${file_size_limit_kb}" ]; then
 
-			if [[ -f "${log_path}.${number_of_logs_to_keep}" ]]; then
-				echo "[info] Deleting oldest log file '${log_path}.${number_of_logs_to_keep}'..."
-				rm -f "${log_path}.${number_of_logs_to_keep}"
-			fi
+		echo "[info] Nginx log file larger than limit ${file_size_limit_kb} kb, rotating logs..."
 
-			for log_number in $(seq "${number_of_logs_to_keep}" -1 0); do
-
-				if [[ -f "${log_path}.${log_number}" ]]; then
-					log_number_inc=$((log_number+1))
-					mv "${log_path}.${log_number}" "${log_path}.${log_number_inc}"
-				fi
-
-			done
-
-			echo "[info] Moving current log to ${log_path}.0..."
-			mv "${log_path}" "${log_path}.0"
-
-			echo "[info] Force Nginx to reload log files..."
-			kill -USR1 $(cat /home/nobody/nginx.pid)
-			sleep 1
-
+		if [[ -f "${log_path}.${number_of_logs_to_keep}" ]]; then
+			echo "[info] Deleting oldest log file '${log_path}.${number_of_logs_to_keep}'..."
+			rm -f "${log_path}.${number_of_logs_to_keep}"
 		fi
 
-		sleep 30s
+		for log_number in $(seq "${number_of_logs_to_keep}" -1 0); do
 
-	done
+			if [[ -f "${log_path}.${log_number}" ]]; then
+				log_number_inc=$((log_number+1))
+				mv "${log_path}.${log_number}" "${log_path}.${log_number_inc}"
+			fi
 
-else
+		done
 
-	echo "[info] Nginx log file does not exist in '${log_path}', skipping log rotation"
+		echo "[info] Moving current log to ${log_path}.0..."
+		mv "${log_path}" "${log_path}.0"
 
-fi
+		echo "[info] Force Nginx to reload log files..."
+		kill -USR1 $(cat /home/nobody/nginx.pid)
+		sleep 1
+
+	fi
+
+	sleep 30s
+
+done
