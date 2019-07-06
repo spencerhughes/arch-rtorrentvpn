0f03b928efeeda66a2efaaacbed1a1883587d3ea
binhex <megalith01@gmail.com>
2019-07-06 23:00:49 +0100

    more specific silence for pyrocore

--- run/nobody/pyrocore.sh
+++ run/nobody/pyrocore.sh
@@ -3,11 +3,11 @@
 if [ ! -d "/config/pyrocore" ]; then
 
 	# define path to pyrocore and create pyrocore config
-	pyroadmin --config-dir="/config/pyrocore" --create-config &>/dev/null
+	pyroadmin --config-dir="/config/pyrocore" --create-config 1> /dev/null
 
 else
 
 	# define path to pyrocore config
-	pyroadmin --config-dir="/config/pyrocore" &>/dev/null
+	pyroadmin --config-dir="/config/pyrocore" 1> /dev/null
 
 fi
