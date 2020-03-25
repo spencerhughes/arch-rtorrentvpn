ead1e04b7d430b4ccfb0aa087b70834c75326632
binhex <megalith01@gmail.com>
2020-03-25 09:41:49 +0000

    additional logging for irssi

--- run/nobody/irssi.sh
+++ run/nobody/irssi.sh
@@ -6,13 +6,19 @@ if [[ "${ENABLE_AUTODL_IRSSI}" == "yes" ]]; then
 	# change directory to script location and then run irssi via tmux
 	cd /home/nobody/.irssi/scripts/autorun
 
+	echo "[info] Attempting to start irssi..."
+
 	# create tmux session name 'irssi_session' and window name 'irssi_window'
 	# note the window number starts at 0
 	/usr/bin/script /home/nobody/typescript --command "/usr/bin/tmux new-session -d -s irssi_session -n irssi_window /usr/bin/irssi"
 
+	echo "[info] irssi process started, updating trackers..."
+
 	# send command to update trackers using tmux send-keys command sent to
 	# the session name and window number (0 in this case)
 	tmux send-keys -t irssi_session:0 "/autodl update" ENTER
+
+	echo "[info] irssi trackers updated"
 
 else
 
